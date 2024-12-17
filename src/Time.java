5bc76e7c732d544dc0979bb2ac1cc3e837567aff
BT_2S14_32 <phoebe19kim@gmail.com>
2024-12-17 10:34:32 -0500

    Finished coding the requirements, now I will start the javadocs! :)
    Finished everything including javadocs! :)

--- src/Time.java
+++ src/Time.java
@@ -1,14 +1,29 @@
+/**
+ * Represents a time on a 24-hour digital clock with hours, minutes, and seconds
+ * Supports time manipulation operations like tick and add
+ */
 public class Time {
     private int hours;
     private int minutes;
     private int seconds;
 
+    /**
+     * Constructs a Time object with specified hours, minutes, and seconds
+     * Assumes input is valid for a 24-hour format
+     *
+     * @param hrs represents the hours (0-23)
+     * @param mins represents the minutes (0-59)
+     * @param secs represents the seconds (0-59)
+     */
     public Time (int hrs, int mins, int secs) {
         this.hours = hrs;
         this.minutes = mins;
         this.seconds = secs;
     }
 
+    /**
+     * Advances the time by one second, handling rollovers for seconds, minutes, and hours
+     */
     public void tick() {
         if (seconds <= 59) {
             seconds += 1;
@@ -29,6 +44,12 @@ public class Time {
         }
     }
 
+    /**
+     * Adds the time from another Time object to this Time object
+     * Changes the state of the current object by adding hours, minutes, and seconds
+     *
+     * @param time represents the Time object to add to the current time
+     */
     public void add(Time time) {
         this.seconds += time.seconds;
         if (this.seconds >= 60) {
@@ -36,13 +57,11 @@ public class Time {
             this.minutes ++;
         }
 
-
         this.minutes += time.minutes;
         if (this.minutes >= 60) {
             this.minutes -= 60;
             this.hours ++;
         }
-
 
         this.hours += time.hours;
         if (this.hours >= 24) {
@@ -50,7 +69,12 @@ public class Time {
         }
     }
 
-
+    /**
+     * Returns a string representation of the time in 00:00:00 format
+     * Ensures each component is two digits with leading 0s if necessary
+     *
+     * @return formatted time string
+     */
     public String toString() {
         String hourStr = String.valueOf(hours);
         if (hours < 10) {
@@ -68,7 +92,6 @@ public class Time {
         if (seconds < 10) {
             secondStr = "0" + secondStr;
         }
-
 
         return hourStr + ":" + minuteStr + ":" + secondStr;
     }
